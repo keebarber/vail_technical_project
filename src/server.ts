52239c759f9d81b7bdea6ab8d36af9c3df6b47d3
Keenan Barber <keenan.414@gmail.com>
2023-03-16 12:46:19 -0400

    Update post method.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import bodyParser from 'body-parser';
 import express from 'express'; 
-import { cors } from 'cors';
-import { morgan } from 'morgan';
+import cors from 'cors';
+import morgan from 'morgan';
 import * as http from 'http';
 import * as dotenv from 'dotenv';
-var packagejson = require('../package.json');
+const querystring = require('querystring')
+const packagejson = require('../package.json');
 
 const app = express();
 const router = express.Router();
@@ -28,14 +29,60 @@ const payload = [
     }
   ];
 
+/*
+*
+* Below is basic router w/ping
+*
+*/
+app.use('/api/v1', router);
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'GET');
+  next();
+});
 
-// defining an endpoint to return all ads
-app.get('/', (req, res) => {
-    res.send(payload);
+
+// Test GET
+router.get('/status', (req, res) => {
+  const data = {
+    uptime: process.uptime(),
+    message: 'Ok',
+    date: new Date()
+  }
+
+  res.status(200).send(data);
+  console.log(JSON.stringify(data))
+});
+
+
+// POST
+app.post('/', (req, res) => {
+
+  // handle errors
+  req.on('error', (err) => {
+    if(err) {
+        res.writeHead(500, {'Content-Type': 'text/html'});
+        res.write('An error occurred');
+        res.end();
+    }
   });
 
+  const echoMsg = JSON.stringify(req.query.message);
+  const payload = {
+    message: 'Ok',
+    packageJsonVersion: packagejson.version,
+    timeStamp: Date.now(),
+    echoMsg,
+    env: JSON.stringify(process.env),
+  }
 
-const port = process.env.PORT || 30000;
-app.listen(port, () => {
-    console.log("Express Server is running on " + port);
+  console.log(JSON.stringify(payload))
+  return res.status(200).send(payload);
 });
+
+
+// Spin up server
+const server = http.createServer(app);
+const port = process.env.PORT || 30000;
+server.listen(port, () => {
+      console.log("Express Server is running on " + port);
+  });
